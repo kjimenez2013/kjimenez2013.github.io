8a2510fe75b9e0be9ffed57f272d2a4148730c6c
Leo Robinovitch <leorobinovitch@gmail.com>
2020-02-29 17:26:48 -0800

    complete top landing

---
+++ assets/js/interaction.js
@@ -0,0 +1,10 @@
+const prevPostButton = document.getElementById("previous")
+const nextPostButton = document.getElementById("next")
+
+prevPostButton.addEventListener("click", () => {
+  console.log("previous post")
+})
+
+nextPostButton.addEventListener("click", () => {
+  console.log("next post")
+})
