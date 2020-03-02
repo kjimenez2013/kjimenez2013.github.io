14691f695046bb0e09c310c1edad88e103353b7a
Leo Robinovitch <leorobinovitch@gmail.com>
2020-03-01 16:48:58 -0800

    add adequate responsiveness for mobile

--- assets/js/interaction.js
+++ assets/js/interaction.js
@@ -1,10 +1,15 @@
-const prevPostButton = document.getElementById("previous")
-const nextPostButton = document.getElementById("next")
+// const prevPostButton = document.getElementById("previous")
+// const nextPostButton = document.getElementById("next")
+// const nestedNavSelect = document.getElementById("nested-nav")
 
-prevPostButton.addEventListener("click", () => {
-  console.log("previous post")
-})
+// prevPostButton.addEventListener("click", () => {
+//   console.log("previous post")
+// })
+//
+// nextPostButton.addEventListener("click", () => {
+//   console.log("next post")
+// })
 
-nextPostButton.addEventListener("click", () => {
-  console.log("next post")
-})
+// nestedNavSelect.addEventListener("change", () => {
+//   console.log(nestedNavSelect.value);
+// })
